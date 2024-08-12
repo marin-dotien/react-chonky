f365f241a19a065eba6aa455733f0161706fcb4f
@m.nevescanin <m.nevescanin@dotien.hr>
2024-08-12 11:27:44 +0200

    Adjust bg color, padding on chonky, add test title

--- packages/chonky/src/components/file-list/ListContainer.tsx
+++ packages/chonky/src/components/file-list/ListContainer.tsx
@@ -15,59 +15,69 @@ import { makeLocalChonkyStyles } from '../../util/styles';
 import { SmartFileEntry } from './FileEntry';
 
 export interface FileListListProps {
-  width: number;
-  height: number;
+    width: number;
+    height: number;
 }
 
 export const ListContainer: React.FC<FileListListProps> = React.memo((props) => {
-  const { width, height } = props;
+    const { width, height } = props;
 
-  const viewConfig = useSelector(selectFileViewConfig);
+    const viewConfig = useSelector(selectFileViewConfig);
 
-  const listRef = useRef<FixedSizeList>();
+    const listRef = useRef<FixedSizeList>();
 
-  const displayFileIds = useSelector(selectors.getDisplayFileIds);
-  const displayFileIdsRef = useInstanceVariable(displayFileIds);
-  const getItemKey = useCallback(
-    (index: number) => displayFileIdsRef.current[index] ?? `loading-file-${index}`,
-    [displayFileIdsRef],
-  );
-
-  const classes = useStyles();
-  const listComponent = useMemo(() => {
-    // When entry size is null, we use List view
-    const rowRenderer = (data: { index: number; style: CSSProperties }) => {
-      return (
-        <div style={data.style}>
-          <SmartFileEntry
-            fileId={displayFileIds[data.index] ?? null}
-            displayIndex={data.index}
-            fileViewMode={FileViewMode.List}
-          />
-        </div>
-      );
-    };
-
-    return (
-      <FixedSizeList
-        ref={listRef as any}
-        className={classes.listContainer}
-        itemSize={viewConfig.entryHeight}
-        height={height}
-        itemCount={displayFileIds.length}
-        width={width}
-        itemKey={getItemKey}
-      >
-        {rowRenderer}
-      </FixedSizeList>
+    const displayFileIds = useSelector(selectors.getDisplayFileIds);
+    const displayFileIdsRef = useInstanceVariable(displayFileIds);
+    const getItemKey = useCallback(
+        (index: number) => displayFileIdsRef.current[index] ?? `loading-file-${index}`,
+        [displayFileIdsRef]
     );
-  }, [classes.listContainer, viewConfig.entryHeight, height, displayFileIds, width, getItemKey]);
 
-  return listComponent;
+    const classes = useStyles();
+    const listComponent = useMemo(() => {
+        // When entry size is null, we use List view
+        const rowRenderer = (data: { index: number; style: CSSProperties }) => {
+            return (
+                <div style={data.style}>
+                    <SmartFileEntry
+                        fileId={displayFileIds[data.index] ?? null}
+                        displayIndex={data.index}
+                        fileViewMode={FileViewMode.List}
+                    />
+                </div>
+            );
+        };
+
+        return (
+            <>
+                <div>Test heading</div>
+                <FixedSizeList
+                    ref={listRef as any}
+                    className={classes.listContainer}
+                    itemSize={viewConfig.entryHeight}
+                    height={height}
+                    itemCount={displayFileIds.length}
+                    width={width}
+                    itemKey={getItemKey}
+                >
+                    {rowRenderer}
+                </FixedSizeList>
+            </>
+        );
+    }, [
+        classes.listContainer,
+        viewConfig.entryHeight,
+        height,
+        displayFileIds,
+        width,
+        getItemKey,
+    ]);
+
+    return listComponent;
 });
 
 const useStyles = makeLocalChonkyStyles((theme) => ({
-  listContainer: {
-    borderTop: `solid 1px ${theme.palette.divider}`,
-  },
+    listContainer: {
+        borderTop: `solid 1px ${theme.palette.divider}`,
+    },
 }));
