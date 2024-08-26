5eee25ef81ab59906375929e8c5d727ba3e8ca03
@m.nevescanin <m.nevescanin@dotien.hr>
2024-08-26 15:38:47 +0200

    Adjust fileEntry to accept columns prop

--- packages/chonky/src/components/file-list/FileEntry.tsx
+++ packages/chonky/src/components/file-list/FileEntry.tsx
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Nullable } from 'tsdef';
 
-import { selectFileData, selectIsDnDDisabled, selectIsFileSelected } from '../../redux/selectors';
+import {
+    selectFileData,
+    selectIsDnDDisabled,
+    selectIsFileSelected,
+} from '../../redux/selectors';
 import { useParamSelector } from '../../redux/store';
 import { DndEntryState, FileEntryProps } from '../../types/file-list.types';
 import { FileViewMode } from '../../types/file-view.types';
@@ -14,71 +18,99 @@ import { DnDFileEntry } from './DnDFileEntry';
 import { useFileClickHandlers } from './FileEntry-hooks';
 import { GridEntry } from './GridEntry';
 import { ListEntry } from './ListEntry';
+import { ColumnDefinition } from './ListContainer';
 
 export interface SmartFileEntryProps {
-  fileId: Nullable<string>;
-  displayIndex: number;
-  fileViewMode: FileViewMode;
+    fileId: Nullable<string>;
+    displayIndex: number;
+    fileViewMode: FileViewMode;
+    columns?: ColumnDefinition[]; // Add columns prop
 }
 
 const disabledDndState: DndEntryState = {
-  dndIsDragging: false,
-  dndIsOver: false,
-  dndCanDrop: false,
+    dndIsDragging: false,
+    dndIsOver: false,
+    dndCanDrop: false,
 };
 
-export const SmartFileEntry: React.FC<SmartFileEntryProps> = React.memo(({ fileId, displayIndex, fileViewMode }) => {
-  const classes = useStyles();
+export const SmartFileEntry: React.FC<SmartFileEntryProps> = React.memo(
+    ({ fileId, displayIndex, fileViewMode, columns }) => {
+        const classes = useStyles();
 
-  // Basic properties
-  const file = useParamSelector(selectFileData, fileId);
-  const selected = useParamSelector(selectIsFileSelected, fileId);
-  const dndDisabled = useSelector(selectIsDnDDisabled);
+        // Basic properties
+        const file = useParamSelector(selectFileData, fileId);
+        const selected = useParamSelector(selectIsFileSelected, fileId);
+        const dndDisabled = useSelector(selectIsDnDDisabled);
 
-  // Clickable wrapper properties
-  const fileClickHandlers = useFileClickHandlers(file, displayIndex);
-  const [focused, setFocused] = useState(false);
-  const clickableWrapperProps: ClickableWrapperProps = {
-    wrapperTag: 'div',
-    passthroughProps: { className: classes.fileEntryClickableWrapper },
-    ...(FileHelper.isClickable(file) ? fileClickHandlers : undefined),
-    setFocused,
-  };
+        // Clickable wrapper properties
+        const fileClickHandlers = useFileClickHandlers(file, displayIndex);
+        const [focused, setFocused] = useState(false);
+        const clickableWrapperProps: ClickableWrapperProps = {
+            wrapperTag: 'div',
+            passthroughProps: { className: classes.fileEntryClickableWrapper },
+            ...(FileHelper.isClickable(file) ? fileClickHandlers : undefined),
+            setFocused,
+        };
 
-  // File entry properties
-  const fileEntryProps: Omit<FileEntryProps, 'dndState'> = {
-    file,
-    selected,
-    focused,
-  };
+        // File entry properties
+        const fileEntryProps: Omit<FileEntryProps, 'dndState'> = {
+            file,
+            selected,
+            focused,
+        };
 
-  let EntryComponent: React.FC<FileEntryProps>;
-  if (fileViewMode === FileViewMode.List) EntryComponent = ListEntry;
-  else if (fileViewMode === FileViewMode.Compact) EntryComponent = CompactEntry;
-  else EntryComponent = GridEntry;
+        // Only change the assignment of EntryComponent
+        let EntryComponent: React.FC<any> = ListEntry;
+        if (fileViewMode === FileViewMode.List) {
+            return dndDisabled ? (
+                <ClickableWrapper {...clickableWrapperProps}>
+                    <ListEntry
+                        {...fileEntryProps}
+                        columns={columns!}
+                        dndState={disabledDndState}
+                    />
+                </ClickableWrapper>
+            ) : (
+                <DnDFileEntry file={file}>
+                    {(dndState) => (
+                        <ClickableWrapper {...clickableWrapperProps}>
+                            <ListEntry
+                                {...fileEntryProps}
+                                columns={columns!}
+                                dndState={dndState}
+                            />
+                        </ClickableWrapper>
+                    )}
+                </DnDFileEntry>
+            );
+        }
 
-  return dndDisabled ? (
-    <ClickableWrapper {...clickableWrapperProps}>
-      <EntryComponent {...fileEntryProps} dndState={disabledDndState} />
-    </ClickableWrapper>
-  ) : (
-    <DnDFileEntry file={file}>
-      {(dndState) => (
-        <ClickableWrapper {...clickableWrapperProps}>
-          <EntryComponent {...fileEntryProps} dndState={dndState} />
-        </ClickableWrapper>
-      )}
-    </DnDFileEntry>
-  );
-});
+        // Fallback for other view modes
+        if (fileViewMode === FileViewMode.Compact) EntryComponent = CompactEntry;
+        else EntryComponent = GridEntry;
+
+        return dndDisabled ? (
+            <ClickableWrapper {...clickableWrapperProps}>
+                <EntryComponent {...fileEntryProps} dndState={disabledDndState} />
+            </ClickableWrapper>
+        ) : (
+            <DnDFileEntry file={file}>
+                {(dndState) => (
+                    <ClickableWrapper {...clickableWrapperProps}>
+                        <EntryComponent {...fileEntryProps} dndState={dndState} />
+                    </ClickableWrapper>
+                )}
+            </DnDFileEntry>
+        );
+    }
+);
+
 SmartFileEntry.displayName = 'SmartFileEntry';
 
 const useStyles = makeGlobalChonkyStyles(() => ({
-  fileEntryClickableWrapper: {
-    // We disable default browser outline because Chonky provides its own outline
-    // (which doesn't compromise accessibility, hopefully)
-    outline: 'none !important',
-    position: 'relative',
-    height: '100%',
-  },
+    fileEntryClickableWrapper: {
+        outline: 'none !important',
+        position: 'relative',
+        height: '100%',
+    },
 }));
